943d21009a4a48da791bdad64aaf6288106e3850
HopMaTuB <sunkiper@gmail.com>
2024-07-22 19:49:18 +0300

    qr code

--- src/routes/qrcode.py
+++ src/routes/qrcode.py
@@ -4,17 +4,22 @@ from src.database.db import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from src.entity.models import Photo
 from src.repository.qr_code import QrCode
+from src.schemas.qr_code import QrSchema,QrUrl
 
 
 
 router = APIRouter(prefix="/generate_qr", tags=["generate_qr"])
 
-@router.get("/generate_qr/{photo_id}")
-async def generate_qr(photo_id: int, db: AsyncSession = Depends(get_db)):
-    photo = await db.query(Photo).filter(Photo.id == photo_id).first()
+@router.post("/generate_qr/{photo_id}", response_model=QrSchema)
+def generate_qr(photo_id: int, db: AsyncSession = Depends(get_db)):
+    photo = db.query(Photo).filter(Photo.id == photo_id).first()
     if not photo:
         raise HTTPException(status_code=404, detail="Photo not found")
     
-    qr_code = await QrCode.generate_qr_code(photo.url,photo.photo_id)
+    qr_code = QrCode.generate_qr_code(photo.url,photo.photo_id)
     return f'{qr_code}'
 
+@router.get('/get_qr/{photo_id}', response_model=QrUrl)
+async def get_qr(photo_id: int, db: AsyncSession = Depends(get_db)):
+    qr_code = await QrCode.get_qr_code(photo_id, db)
+    return {'qr_url': qr_code}
