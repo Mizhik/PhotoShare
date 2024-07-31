2ae9e3cda33a17dd920002d47472aaaca360ed29
sardinka17 <sardinka117@gmail.com>
2024-07-31 12:55:24 +0300

    Extended models to remove related tables.

--- src/entity/models.py
+++ src/entity/models.py
@@ -12,10 +12,10 @@ class Base(DeclarativeBase):
 
 
 photo_tag_association = Table(
-    "photo_tag",
-    Base.metadata,
-    Column("photo_id", PGUUID(as_uuid=True), ForeignKey("photos.id")),
-    Column("tag_id", PGUUID(as_uuid=True), ForeignKey("tags.id")),
+	"photo_tag",
+	Base.metadata,
+	Column("photo_id", PGUUID(as_uuid=True), ForeignKey("photos.id")),
+	Column("tag_id", PGUUID(as_uuid=True), ForeignKey("tags.id")),
 )
 
 
@@ -34,9 +34,16 @@ class User(Base):
 	created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
 	updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
 	role: Mapped[Role] = mapped_column("role", Enum(Role), default=Role.user)
-	photos: Mapped[list['Photo']] = relationship('Photo', back_populates='user', lazy="selectin")
-	comments: Mapped[list['Comment']] = relationship('Comment', back_populates='user', lazy="selectin")
-	ratings: Mapped[list['Rating']] = relationship('Rating', back_populates='user', lazy="selectin")
+	photos: Mapped[list['Photo']] = relationship(
+		'Photo', back_populates='user', cascade="all, delete-orphan", lazy="selectin"
+	)
+	comments: Mapped[list['Comment']] = relationship(
+		'Comment', back_populates='user', cascade="all, delete-orphan", lazy="selectin"
+	)
+	ratings: Mapped[list['Rating']] = relationship(
+		'Rating', back_populates='user', cascade="all, delete-orphan", lazy="selectin"
+	)
+
 	@property
 	def is_admin(self):
 		return self.role == Role.admin
@@ -44,6 +51,7 @@ class User(Base):
 	@property
 	def is_moderator(self):
 		return self.role == Role.moderator
+
 
 class Photo(Base):
 	__tablename__ = 'photos'
@@ -54,19 +62,31 @@ class Photo(Base):
 	user_id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), ForeignKey('users.id'))
 	created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
 	updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
-	tags: Mapped[list['Tag']] = relationship('Tag', secondary=photo_tag_association, back_populates='photos', lazy="selectin")
-	transformed_images: Mapped[list['TransformedImage']] = relationship('TransformedImage', back_populates='photo', lazy="selectin")
-	qr_code: Mapped[list['QrCode']] = relationship('QrCode', back_populates='qr_code', lazy="selectin")
-	comments: Mapped[list['Comment']] = relationship('Comment', back_populates='photo',cascade="all, delete-orphan", lazy="selectin")
+	tags: Mapped[list['Tag']] = relationship(
+		'Tag', secondary=photo_tag_association, back_populates='photos', lazy="selectin"
+	)
+	transformed_images: Mapped[list['TransformedImage']] = relationship(
+		'TransformedImage', back_populates='photo', cascade="all, delete-orphan", lazy="selectin"
+	)
+	qr_code: Mapped[list['QrCode']] = relationship(
+		'QrCode', back_populates='qr_code', cascade="all, delete-orphan", lazy="selectin"
+	)
+	comments: Mapped[list['Comment']] = relationship(
+		'Comment', back_populates='photo', cascade="all, delete-orphan", lazy="selectin"
+	)
 	user: Mapped['User'] = relationship('User', back_populates='photos', lazy="selectin")
-	ratings: Mapped[list['Rating']] = relationship('Rating', back_populates='photo', lazy="selectin")
+	ratings: Mapped[list['Rating']] = relationship(
+		'Rating', back_populates='photo', cascade="all, delete-orphan", lazy="selectin"
+	)
 
 
 class Tag(Base):
 	__tablename__ = "tags"
 	id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), primary_key=True, default=uuid4)
 	name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
-	photos: Mapped[list['Photo']] = relationship('Photo', secondary=photo_tag_association, back_populates='tags', lazy="selectin")
+	photos: Mapped[list['Photo']] = relationship(
+		'Photo', secondary=photo_tag_association, back_populates='tags', lazy="selectin"
+	)
 
 
 class Comment(Base):
@@ -88,6 +108,7 @@ class TransformedImage(Base):
 	transformed_url: Mapped[str] = mapped_column(String, nullable=True)
 	photo: Mapped['Photo'] = relationship('Photo', back_populates='transformed_images', lazy="selectin")
 
+
 class QrCode(Base):
 	__tablename__ = 'qr_codes'
 	id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), primary_key=True, default=uuid4)
@@ -95,11 +116,12 @@ class QrCode(Base):
 	qr_code_url: Mapped[str] = mapped_column(String, nullable=True)
 	qr_code: Mapped['Photo'] = relationship('Photo', back_populates='qr_code', lazy="selectin")
 
+
 class Rating(Base):
-    __tablename__ = "ratings"
-    id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), primary_key=True, default=uuid4)
-    photo_id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), ForeignKey("photos.id"))
-    user_id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), ForeignKey("users.id"))
-    rating: Mapped[int] = mapped_column(Integer, nullable=False)
-    photo: Mapped["Photo"] = relationship("Photo", back_populates="ratings", lazy="selectin")
-    user: Mapped["User"] = relationship("User", back_populates="ratings", lazy="selectin")
+	__tablename__ = "ratings"
+	id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), primary_key=True, default=uuid4)
+	photo_id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), ForeignKey("photos.id"))
+	user_id: Mapped[UUID] = mapped_column(PGUUID(as_uuid=True), ForeignKey("users.id"))
+	rating: Mapped[int] = mapped_column(Integer, nullable=False)
+	photo: Mapped["Photo"] = relationship("Photo", back_populates="ratings", lazy="selectin")
+	user: Mapped["User"] = relationship("User", back_populates="ratings", lazy="selectin")
